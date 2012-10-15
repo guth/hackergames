591603eca82cef48b2db9e499b55caecbf0db23d
guth <guthriea90@gmail.com>
2012-10-15 15:28:14 -0400

    Documentation and general clean up.

--- Solution.java
+++ Solution.java
@@ -16,8 +16,8 @@ public class Solution
 	private final int C = 20;
 	private final int NUM_COPS = 3;
 
-	private final int[] robberDR = {-1, -1, -1, 0, 0,  1, 1, 1};
-	private final int[] robberDC = {-1,  0,  1,-1, 1, -1, 0, 1};
+	private final int[] robberDR = {0, -1, -1, -1, 0, 0,  1, 1, 1};
+	private final int[] robberDC = {0, -1,  0,  1,-1, 1, -1, 0, 1};
 
 	private final int[] copDR = {0, -1,  0, 0, 1};
 	private final int[] copDC = {0,  0, -1, 1, 0};
@@ -25,182 +25,50 @@ public class Solution
 	private char PLAYER;
 	private char OPPONENT;
 	
-	private boolean DEBUG = false;
-	
+	/**
+	 * Makes a move.
+	 * Reads input in the form of '%c %d %d %d %d %d %d %d %d'
+	 * The first character is the player to perform a move for (R for robber, C for cops)
+	 * The next two integers is the row and column of the robber.
+	 * The next six integers is the row and columns of each of the cops.
+	 * If the player is the cops, then the six integers are the current positions of their cops.
+	 * If the player is the robber, then the positions are the last known locations of the cops.
+	 */
 	public void go()
 	{
-		int[] a;
-		Arrays.sort(a)
-		
-		List<Integer> b;
-		Collections.sort(b);
-	
 		Scanner s = new Scanner(System.in);
 
 		char player = s.next().trim().charAt(0);
 		PLAYER = player;
 		OPPONENT = (PLAYER == COP) ? ROBBER : COP;
-		//System.err.println("Player: " + PLAYER);
-		//System.err.println("Opponent: " + OPPONENT);
 
 		Point robber = new Point(s.nextInt(), s.nextInt());
-		//System.err.println("Robber is at: " + "(" + robber.r + ", " + robber.c + ")");
+		
 		Point[] cops = new Point[NUM_COPS];
 		for(int k=0; k<NUM_COPS; k++)
 		{
 			cops[k] = new Point(s.nextInt(), s.nextInt());
-			//System.err.println("Cop #" + k + ": (" + cops[k].r + ", " + cops[k].c + ")");
 		}
-		//ClosestData close = getClosestData(robber, cops);
-		//printMatrix(close.matrix);
 
 		if(player == ROBBER)
 		{
-			//alphabeta(robber, cops, DEPTH, Integer.MIN_VALUE, Integer.MAX_VALUE, player);
-			//Point p = globalBestRobber;
 			Point p = doRobberMove(robber, cops);
-			System.out.println(p.r + " " + p.c);
+			System.out.printf("%d %d\n", p.r, p.c);
 		}
 		else if(player == COP)
 		{
-//			Point[] p = globalBestCops;
 			Point[] p = doCopMove(robber, cops);
 			System.out.printf("%d %d %d %d %d %d\n",
 						p[0].r, p[0].c, p[1].r, p[1].c, p[2].r, p[2].c);
 		}
-
-		// if(player == ROBBER)
-		// {
-		// 	Point p = doRobberMove(robber, cops);
-		// 	System.out.println(p.r + " " + p.c);
-		// }
-		// else if(player == COP)
-		// {
-		// 	Point[] p = doCopMove(robber, cops);
-		// 	System.out.printf("%d %d %d %d %d %d\n",
-		// 				p[0].r, p[0].c, p[1].r, p[1].c, p[2].r, p[2].c);
-		// }
-	}
-/*
-	function alphabeta(node, depth, a, b, Player)         
-	    if  depth = 0 or node is a terminal node
-	        return the heuristic value of node
-	    if  Player = MaxPlayer
-	        for each child of node
-	            a := max(a, alphabeta(child, depth-1, a, b, not(Player) ))     
-	            if b <= a
-	                break                             (* Beta cut-off *)
-	        return a
-	    else
-	        for each child of node
-	            b := min(b, alphabeta(child, depth-1, a, b, not(Player) ))     
-	            if b <= a
-	                break                             (* Alpha cut-off *)
-	        return b
-	
-	(* Initial call *)
-	alphabeta(origin, depth, -infinity, +infinity, MaxPlayer)
-*/
-	private Point globalBestRobber = null;
-	private Point[] globalBestCops = null;
-	private int alphabeta(Point robber, Point[] cops, int depth, int alpha, int beta, char player)
-	{
-		char otherPlayer = (player == COP) ? ROBBER : COP;
-
-		//if(depth == 0 || (player == COP && robberIsCaught(robber, cops)))
-		if(depth == 0 || robberIsCaught(robber, cops))
-		{
-			int val = eval(player, robber, cops);
-			return val;
-		}
-
-		Point bestRobber = null;
-		Point[] bestCops = null;
-		if(player == PLAYER) // max player
-		{
-			if(player == COP)
-			{
-				List<Point[]> newCopsList = getNextCopStates(cops);
-				for(Point[] newCops : newCopsList)
-				{
-					int temp = alphabeta(robber, newCops, depth-1, alpha, beta, otherPlayer);
-					if(temp > alpha)
-					{
-						alpha = temp;
-						bestCops = newCops;
-					}
-					if(temp == alpha && bestCops == null)
-						bestCops = newCops;
-					if(beta <= alpha)
-						break;
-				}
-			}
-			else if(player == ROBBER)
-			{
-				List<Point> newRobberList = getNextRobberStates(robber);
-				for(Point newRobber : newRobberList)
-				{
-					int temp = alphabeta(newRobber, cops, depth-1, alpha, beta, otherPlayer);
-					if(temp > alpha)
-					{
-						alpha = temp;
-						bestRobber = newRobber;
-					}
-					if(temp == alpha && bestRobber == null)
-						bestRobber = newRobber;
-					if(beta <= alpha)
-						break;
-				}
-			}
-			globalBestCops = bestCops;
-			globalBestRobber = bestRobber;
-			return alpha;
-		}
-		else //otherplayer == PLAYER // min player
-		{
-			if(otherPlayer == COP)
-			{
-				List<Point[]> newCopsList = getNextCopStates(cops);
-				for(Point[] newCops : newCopsList)
-				{
-					int temp = alphabeta(robber, newCops, depth-1, alpha, beta, otherPlayer);
-
-					if(temp < beta)
-					{
-						beta = temp;
-						bestCops = newCops;
-					}
-					if(temp == beta && bestCops == null)
-						bestCops = newCops;
-					if(beta <= alpha)
-						break;
-				}
-			}
-			else if(otherPlayer == ROBBER)
-			{
-				List<Point> newRobberList = getNextRobberStates(robber);
-				for(Point newRobber : newRobberList)
-				{
-					int temp = alphabeta(newRobber, cops, depth-1, alpha, beta, otherPlayer);
-					beta = Math.min(beta, temp);
-					if(temp < beta)
-					{
-						beta = temp;
-						bestRobber = newRobber;
-					}
-					if(temp == beta && bestRobber == null)
-						bestRobber = newRobber;
-					if(beta <= alpha)
-						break;
-				}
-			}
-			globalBestCops = bestCops;
-			globalBestRobber = bestRobber;
-			return beta;
-		}
 	}
 
-	// Moves to a random point that's not a cop
+	/**
+	 * Moves the robber to the nearest point with the highest weight.
+	 * Weight is calculated as the number of squares the robber can reach
+	 * from that state before all the cops.
+	 * The robber prefers states that are at least 2 squares away from any cop.
+	 */
 	private Point doRobberMove(Point robber, Point[] cops)
 	{
 		List<Point> newRobberList = getNextRobberStates(robber);
@@ -253,118 +121,89 @@ public class Solution
 		}
 		
 		return best;
-	/*
-		Point ret = null;
-		loop: while(ret == null)
-		{
-			Random rnd = new Random();
-			int val = rnd.nextInt(8); 	// Between [0, 8)
-
-			if(val < 0 || val >= 8)
-				continue;
-			int dr = robberDR[val];
-			int dc = robberDC[val];
-
-			int nr = robber.r + dr;
-			int nc = robber.c + dc;
-			if(isInBounds(nr, nc))
-			{
-				for(Point cop : cops)
-				{
-					if(nr == cop.r && nc == cop.c)
-						continue loop;
-				}
-				ret = new Point(nr, nc);
-			}
-		}
-		return ret;
-		*/
 	}
 
-	// Collectively moves towards the robber
+	/**
+	 * Moves the cops.
+	 * If the cops are not in the horizontal line, shift into one.
+	 * Once in a horizontal line, the cops will progressively move
+	 * towards the robber, keeping the robber centered when possible.
+	 */
 	private Point[] doCopMove(Point robber, Point[] cops)
 	{
 		Point[] ret = new Point[NUM_COPS];
 
+		boolean allZero = true;
 		for(int k=0; k<NUM_COPS; k++)
 		{
-			int rDiff = robber.r - cops[k].r;
-			int cDiff = robber.c - cops[k].c;
-
-			int dr = rDiff == 0 ? 0 : rDiff/Math.abs(rDiff);
-			int dc = cDiff == 0 ? 0 : cDiff/Math.abs(cDiff);
-
-			if(k == 0)
+			if(cops[k].r + cops[k].c != 0)
 			{
-				// if(Math.abs(rDiff) > Math.abs(cDiff))
-				// 	ret[k] = new Point(cops[k].r+dr, cops[k].c);
-				// else
-				// 	ret[k] = new Point(cops[k].r, cops[k].c+dc);
-				if(dr == 0)
-					ret[k] = new Point(cops[k].r, cops[k].c+dc);
-				else
-					ret[k] = new Point(cops[k].r+dr, cops[k].c);
-			}
-			else if(k==1)
-			{
-				if(dc == 0)
-					ret[k] = new Point(cops[k].r+dr, cops[k].c);
-				else
-					ret[k] = new Point(cops[k].r, cops[k].c+dc);
-			}
-			else // k == 2
-			{
-				if(Math.abs(rDiff) > Math.abs(cDiff))
-					ret[k] = new Point(cops[k].r+dr, cops[k].c);
-				else
-					ret[k] = new Point(cops[k].r, cops[k].c+dc);
+				allZero = false;
+				break;
 			}
 		}
+
+		if(allZero)
+		{
+			// First turn, all are at the origin
+			cops[2] = new Point(0, 1);
+			return cops;
+		}
+		else if(cops[0].r+cops[0].c==0 && cops[1].r+cops[1].c==0 && cops[2].r==0 && cops[2].c==1)
+		{
+			// Second turn. First 2 are at the origin, 3rd cop is at (0,1)
+			cops[1] = new Point(0,1);
+			cops[2] = new Point(0,2);
+			return cops;
+		}
+
+		// Cops are in a horizontal row. Now do stuff.
+		int rDiff = robber.r - cops[1].r;
+		int cDiff = robber.c - cops[1].c;
+
+		int dr = rDiff == 0 ? 0 : rDiff/Math.abs(rDiff);
+		int dc = cDiff == 0 ? 0 : cDiff/Math.abs(cDiff);
+
+		Point midCop = cops[1];
+		// If robber is exactly one above or below the middle cop
+		if(robber.c==midCop.c && robber.r==midCop.r+dr)
+		{
+			double rand = Math.random();
+			if(rand <= 0.5)
+				return cops;
+		}
+
+		if(robber.c==midCop.c)
+		{
+			for(int k=0; k<NUM_COPS;k++)
+			{
+				ret[k] = new Point(cops[k].r+dr, cops[k].c);
+			}
+		}
+		else
+		{
+			if(rDiff > cDiff)
+			{
+				for(int k=0; k<NUM_COPS; k++)
+				{
+					ret[k] = new Point(cops[k].r+dr, cops[k].c);
+				}
+			}
+			else // rDiff <= cDiff
+			{
+				for(int k=0; k<NUM_COPS; k++)
+				{
+					ret[k] = new Point(cops[k].r, cops[k].c+dc);
+				}
+			}
+		}
+
 		return ret;
 	}
 
-	// Always higher value for the current player.
-	private int eval(char player, Point robber, Point[] cops)
-	{
-		ClosestData data = getClosestData(robber, cops);
-		
-		if(player == COP)
-		{
-			int totalDistFromRobber = 0;
-			for(int k=0; k<NUM_COPS; k++)
-			{
-				totalDistFromRobber += copDist(cops[k], robber.r, robber.c);
-			}
-			int avgDistFromRobber = totalDistFromRobber / NUM_COPS;
-			return 1000-totalDistFromRobber;
-		}
-		else //if(player == ROBBER)
-		{
-			if(robberIsCaught(robber, cops))
-				return Integer.MIN_VALUE;
-			//int distFromCenter = Math.abs(robber.r-R/2)+Math.abs(robber.c-C/2);
-			
-			//return totalDistFromCops;
-			return data.numRobberCloser;
-		}
-//		int diff = data.numCopsCloser - data.numRobberCloser;
-
-//		// High is good for robber, bad for cops
-//		int distToRobber = minCopDist(cops, robber.r, robber.c);
-//
-//		if(player == ROBBER)
-//		{
-//			diff = -diff;
-//			diff += distToRobber;
-//		}
-//		else // player == cop
-//		{
-//			diff -= distToRobber;
-//		}
-//
-//		return diff;
-	}
-
+	/**
+	 * Returns the list of all possible states for the robber to transition into.
+	 */
 	private List<Point> getNextRobberStates(Point robber)
 	{
 		List<Point> ret = new ArrayList<Point>();
@@ -380,6 +219,11 @@ public class Solution
 		return ret;
 	}
 
+	/**
+	 * Returns the list of possible states for the cops to transition into.
+	 * Does not deal with duplicates (i.e. 2 cops switching positions is
+	 * stored twice as two different states)
+	 */
 	private List<Point[]> getNextCopStates(Point[] cops)
 	{
 		List<Point[]> ret = new ArrayList<Point[]>();
@@ -411,6 +255,11 @@ public class Solution
 		return ret;
 	}
 
+	/**
+	 * Calculates and returns the ClosestData for the given robber and cops.
+	 * ClosestData contains information about which player reaches each square
+	 * first and how many squares each player can reach before the other player.
+	 */
 	private ClosestData getClosestData(Point robber, Point[] cops)
 	{
 		char[][] matrix = new char[R][C];
@@ -442,11 +291,20 @@ public class Solution
 		return new ClosestData(matrix, numRobber, numCop);
 	}
 
+	
+	/**
+	 * Returns the number of steps it will take the given cop
+	 * to reach the given position.
+	 */
 	private int copDist(Point cop, int r, int c)
 	{
 		return (Math.abs(cop.r-r) + Math.abs(cop.c-c));
 	}
 
+	/**
+	 * Returns the number of steps it will take the closest cop
+	 * to reach the given position.
+	 */
 	private int minCopDist(Point[] cops, int r, int c)
 	{
 		int min = Integer.MAX_VALUE;
@@ -457,21 +315,35 @@ public class Solution
 		return min;
 	}
 
+	/**
+	 * Returns the number of steps it will take the robber to reach
+	 * the given position.
+	 */
 	private int robberDist(Point robber, int r, int c)
 	{
 		return Math.max(Math.abs(robber.r-r), Math.abs(robber.c-c));
 	}
 
+	/**
+	 * Returns true if the row and column is in bounds, false otherwise.
+	 */
 	private boolean isInBounds(int r, int c)
 	{
 		return (r >= 0 && c >= 0 && r < R && c < C);
 	}
 
+	/**
+	 * Returns true if the point is in bounds, false otherwise.
+	 */
 	private boolean isInBounds(Point p)
 	{
 		return isInBounds(p.r, p.c);
 	}
 
+	/*
+	 * Returns true if the robber is currently standing on any
+	 * of the cops, false otherwise.
+	 */
 	private boolean robberIsCaught(Point robber, Point[] cops)
 	{
 		for(Point cop : cops)
@@ -482,6 +354,9 @@ public class Solution
 		return false;
 	}
 	
+	/**
+	 * Prints the given character array.
+	 */
 	private void printMatrix(char[][] matrix)
 	{
 		for(int r=0; r<R; r++)
@@ -494,6 +369,9 @@ public class Solution
 		}
 	}
 
+	/**
+	 * Simple Point class with a row and column.
+	 */
 	public class Point
 	{
 		public int r;
@@ -512,6 +390,10 @@ public class Solution
 		}
 	}
 
+	/**
+	 * Contains a grid that shows which player can get to each square the quickest
+	 * and the total number of squares each player can reach first.
+	 */
 	public class ClosestData
 	{
 		public char[][] matrix;
